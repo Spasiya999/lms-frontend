ac22ae97048d3a853e0dece762abe372f2f5861c
PasinduGui <sachintha@guisrilanka.com>
2024-09-02 13:22:09 +0530

    upload dist file

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store';
-import router from './router';
-
-import '../public/css/style.css';
-
-createApp(App).use(store).use(router).mount('#app')
